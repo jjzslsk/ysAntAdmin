d426f134a8c5cee90f7a835d8c78e9cd50398311
jjzslsk <3702740@qq.com>
2019-01-18 02:35:22 +0800

    更新

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,11 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_API: '"http://192.168.99.123:1030/api"' //注入线上api的根路径
+  BASE_API: '"http://192.168.99.123:1030/api"' //注入线上api的根路径
   // BASE_API: '"http://localhost:668/api"' //注入线上api的根路径
   // BASE_API: '"http://192.168.0.150:668/api"' //注入线上api的根路径
   // BASE_API: '"http://192.168.0.150:966/api"' //注入线上api的根路径
   // BASE_API: '"http://192.168.0.123:8066/api"' //注入线上api的根路径
-  BASE_API: '"http://jjzslsk.gicp.net:37537/api"' //注入线上api的根路径
+  // BASE_API: '"http://jjzslsk.gicp.net:37537/api"' //注入线上api的根路径
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
 }
